3137f724f6d4838e016d111a5748d18da148dcf8
Dan <>
2018-04-15 15:01:26 -0700

    Added test data and a simple tree model, output and writing to sample file for submission (it scored a 0.6980).

--- Water_Table/WaterTable.R
+++ Water_Table/WaterTable.R
@@ -2,27 +2,35 @@ setwd("~/Documents/Predict 498/MSDS_Capstone/")
 water_table <- read.csv("Water_Table/tanzania-X-train.csv")
 water_table_y <- read.csv("Water_Table/tanzania-y-train.csv")
 
+water_table_test <- read.csv("Water_Table/tanzania-X-test.csv")
 library(lubridate)
 
 sum(water_table$population)
 class(water_table$has_amount_tsh)
 water_table$y <- water_table_y$status_group
+maxYear <- 2013
+
+cleanData <- function(data) {
+  data$date_recorded <- as.Date(data$date_recorded) 
+  data$age <- 0
+  data[data$construction_year>0,]$age <- maxYear -  data[data$construction_year>0,]$construction_year
+  
+  data$has_population <- 0
+  data[data$population>0,]$has_population <- 1
+  
+  data$has_amount_tsh <- 0
+  data[data$amount_tsh>0,]$has_amount_tsh <- 1
+  
+  data$has_construction_year <- 1
+  data[data$construction_year==0,]$has_construction_year <- 0 
+  
+  return(data)
+}
+
+water_table <- cleanData(water_table)
+water_table_test <- cleanData(water_table_test)
 
 
-water_table$date_recorded <- as.Date(water_table$date_recorded) 
-
-maxYear <- max(water_table$construction_year)
-water_table$age <- NA
-water_table[water_table$construction_year>0,]$age <- maxYear -  water_table[water_table$construction_year>0,]$construction_year
-
-water_table$has_population <- 0
-water_table[water_table$population>0,]$has_population <- 1
-
-water_table$has_amount_tsh <- 0
-water_table[water_table$amount_tsh>0,]$has_amount_tsh <- 1
-
-water_table$has_construction_year <- 1
-water_table[water_table$construction_year==0,]$has_construction_year <- 0 
 
 #install.packages("tree")
 library(tree)
@@ -45,31 +53,16 @@ plot(tree.donr)
 text(tree.donr, pretty=0) 
 
 
+status_group <- predict(tree.donr, water_table_test, type="class")
 
 
-library(MASS)
-##### CLASSIFICATION MODEL - LINEAR DISCRIMINANT ANALYSIS
-model.lda1 <- lda(y ~ 
-                    has_amount_tsh + 
-                    has_construction_year + 
-                    age + 
-                    source + 
-                    management + 
-                    water_quality + 
-                    quantity + 
-                    extraction_type + 
-                    extraction_type_group + 
-                    extraction_type_class + 
-                    source_type + 
-                    permit +
-                    population 
-                  ,data=water_table) 
+simpleTree <- data.frame(water_table_test$id, status_group)
+colnames(simpleTree) = c("id","status_group")
+write.csv(simpleTree, file="Water_Table//simpleTree.csv", row.names=FALSE) 
 
-hist(log(water_table[water_table$has_amount_tsh==1,]$amount_tsh + 1))
 
-head(water_table[,c(2,24,41, 42)])
-#kdepairs(water_table[,c(2,24,42, 43)], pch=19)
 
-summary(water_table[water_table$has_population==1,])
+
+
 
 

2c7c292a05bec970e38c79fa09d762f9fdbb1008
Tine Hutchison <github@well-duh.net>
2018-04-29 16:13:43 -0700

    got it working. Now just playing with network configuration.

--- Water_Table/tz_tensorflow.py
+++ Water_Table/tz_tensorflow.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from tensorflow.python import debug as tf_debug
 
 
 def do_transformations(df):
@@ -15,11 +16,12 @@ def do_transformations(df):
                   'region': transform_region, 'lga': transform_lga, 'ward': transform_ward,
                   'population': transform_population, 'public_meeting': transform_public_meeting,
                   'recorded_by': transform_recorded_by, 'scheme_management': transform_scheme_management,
-                  'permit': transform_permit, 'construction_year': transform_construction_year,
-                  'extraction_type': transform_extraction_type, 'extraction_type_group': transform_extraction_type_group,
-                  'management': transform_management, 'management_group': transform_management_group,
-                  'payment': transform_payment, 'water_quality': transform_water_quality,
-                  'quality_group': transform_quality_group, 'quantity': transform_quantity, 'source': transform_source,
+                  'scheme_name': transform_scheme_name, 'permit': transform_permit,
+                  'construction_year': transform_construction_year, 'extraction_type': transform_extraction_type,
+                  'extraction_type_group': transform_extraction_type_group, 'management': transform_management,
+                  'management_group': transform_management_group, 'payment': transform_payment,
+                  'water_quality': transform_water_quality, 'quality_group': transform_quality_group,
+                  'quantity': transform_quantity, 'source': transform_source,
                   'source_class': transform_source_class, 'waterpoint_type': transform_waterpoint_type}
 
     drop_cols = ['wpt_name', 'num_private', 'subvillage', 'region_code', 'district_code', 'extraction_type_class',
@@ -32,7 +34,7 @@ def do_transformations(df):
         df[col] = dispatcher[col](df, col)
 
     #Create an 'age' categorical column 2018 - construction_year. If construtction = 0 mark as '>52'
-    df['age'] = df.construction_year.apply(lambda x: 'unknown' if x == 0 else 2018 - x).astype('category')
+    # df['age'] = df.construction_year.apply(lambda x: 'unknown' if x == 0 else 2018 - x).astype('category')
 
     return df
 
@@ -82,11 +84,11 @@ def transform_latitude(df, col):
 
 
 def transform_basin(df, col):
-    return df[col].astype('category')
+    return df[col]
 
 
 def transform_region(df, col):
-    return df[col].astype('category')
+    return df[col]
 
 
 def transform_lga(df, col):
@@ -105,10 +107,14 @@ def transform_public_meeting(df, col):
 
 
 def transform_recorded_by(df, col):
-    return df[col].fillna('Other').astype('category')
+    return df[col].fillna('Other')
 
 def transform_scheme_management(df, col):
-    return df[col].fillna('Other').astype('category')
+    return df[col].fillna('Other')
+
+
+def transform_scheme_name(df, col):
+    return df[col].fillna('Unknown')
 
 
 def transform_permit(df, col):
@@ -121,47 +127,47 @@ def transform_construction_year(df, col):
 
 
 def transform_extraction_type(df, col):
-    return df[col].fillna('Other').astype('category')
+    return df[col].fillna('Other')
 
 
 def transform_extraction_type_group(df, col):
-    return df[col].fillna('Other').astype('category')
+    return df[col].fillna('Other')
 
 
 def transform_management(df, col):
-    return df[col].fillna('Other').astype('category')
+    return df[col].fillna('Other')
 
 
 def transform_management_group(df, col):
-    return df[col].fillna('Other').astype('category')
+    return df[col].fillna('Other')
 
 
 def transform_payment(df, col):
-    return df[col].fillna('other').astype('category')
+    return df[col].fillna('other')
 
 
 def transform_water_quality(df, col):
-    return df[col].fillna('unknown').astype('category')
+    return df[col].fillna('unknown')
 
 
 def transform_quality_group(df, col):
-    return df[col].fillna('unknown').astype('category')
+    return df[col].fillna('unknown')
 
 
 def transform_quantity(df, col):
-    return df[col].fillna('unknown').astype('category')
+    return df[col].fillna('unknown')
 
 
 def transform_source(df, col):
-    return df[col].fillna('unknown').astype('category')
+    return df[col].fillna('unknown')
 
 
 def transform_source_class(df, col):
-    return df[col].fillna('unknown').astype('category')
+    return df[col].fillna('unknown')
 
 
 def transform_waterpoint_type(df, col):
-    return df[col].fillna('other').astype('category')
+    return df[col].fillna('other')
 
 
 
@@ -169,11 +175,11 @@ current_dir = '/home/tine/PycharmProjects/pred498/MSDS_Capstone/Water_Table/'
 #load training data
 train_data = do_transformations(pd.read_csv(current_dir + 'tanzania-X-train.csv', header=0))
 train_target = pd.read_csv(current_dir + 'tanzania-y-train.csv', header=0)
-status_dict = {'functional': 0,
-               'non functional': 1,
-               'functional needs repair': 2}
-train_target.status_group = train_target.status_group.apply(lambda x: status_dict[x])
-train_target.status_group = train_target.status_group.astype('int')
+# status_dict = {'functional': 0,
+#                'non functional': 1,
+#                'functional needs repair': 2}
+# train_target.status_group = train_target.status_group.apply(lambda x: status_dict[x])
+# train_target.status_group = train_target.status_group.astype('int')
 
 #load testing data
 test_data = do_transformations(pd.read_csv(current_dir + 'tanzania-x-test.csv', header=0))
@@ -181,20 +187,8 @@ test_data = do_transformations(pd.read_csv(current_dir + 'tanzania-x-test.csv', 
 # Create a list to hold tensorflow feature columns
 feature_columns = list()
 
-"""
-['id', 'amount_tsh', 'date_recorded', 'funder', 'gps_height',
-       'installer', 'longitude', 'latitude', 'wpt_name', 'num_private',
-       'basin', 'subvillage', 'region', 'region_code', 'district_code', 'lga',
-       'ward', 'population', 'public_meeting', 'recorded_by',
-       'scheme_management', 'scheme_name', 'permit', 'construction_year',
-       'extraction_type', 'extraction_type_group', 'extraction_type_class',
-       'management', 'management_group', 'payment', 'payment_type',
-       'water_quality', 'quality_group', 'quantity', 'quantity_group',
-       'source', 'source_type', 'source_class', 'waterpoint_type',
-       'waterpoint_type_group']
-"""
 # Create numeric feature columns
-for key in  ['amount_tsh', 'gps_height','longitude','latitude']:
+for key in ['amount_tsh', 'gps_height', 'longitude', 'latitude', 'population', 'construction_year']:
     feature_columns.append(tf.feature_column.numeric_column(key=key))
 
 # Create categorical feature columns for categories with less than 30 possible values
@@ -205,8 +199,8 @@ for key in [x for x in object_vars if len(train_data[x].unique()) <= 30]:
     if key == 'date_recorded':
         continue
     feature_columns.append(tf.feature_column.indicator_column(
-                            tf.feature_column.categorical_column_with_vocabulary_list(
-                            key, vocabulary_list=list(train_data[key].unique())
+                             tf.feature_column.categorical_column_with_vocabulary_list(
+                               key, vocabulary_list=list(train_data[key].unique())
     )))
 
 # Create hashed feature columns for categories with more than 30 unique values
@@ -215,6 +209,11 @@ for key in [x for x in object_vars if len(train_data[x].unique()) > 30]:
         continue
     feature_columns.append(tf.feature_column.indicator_column(
                             tf.feature_column.categorical_column_with_hash_bucket(key, hash_bucket_size=100)))
+
+
+# Create input dataset
+hooks = [tf_debug.LocalCLIDebugHook()]
+
 
 # Generate list of column names actually used
 feature_names = list()
@@ -225,20 +224,34 @@ for key in feature_columns:
     else:
         feature_names.append(key.key)
 
-# Create input dataset
+result_list = list()
+for x in range(10,100,5):
+    for x2 in range(8,24):
+        local_results = [x, x2]
+        hidden_layers = [x, x2]
+        pdinput = tf.estimator.inputs.pandas_input_fn(train_data[feature_names],
+                                                    train_target.status_group,
+                                                    batch_size=100,
+                                                    shuffle=False)
 
-input = tf.estimator.inputs.pandas_input_fn(train_data[feature_names],
-                                            train_target.status_group, shuffle=False)
+        classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
+                                                hidden_units=hidden_layers,
+                                                n_classes=3,
+                                                label_vocabulary=list(train_target.status_group.unique()))
 
-classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
-                                        hidden_units=[30,10],
-                                        n_classes=3)
+        classifier.train(input_fn=pdinput)
 
-#train_target['new_status'] = train_target.status_group.apply(lambda x: 1 if x == 'functional' else 2 if x == 'functional needs repair')
-classifier.train(input_fn=input)
+        result_list.append(local_results.append(classifier.evaluate(input_fn=pdinput)))
 
-eval_result = classifier.evaluate(input_fn=input)
 
-test_data = do_transformations(test_data)
-test_input = tf.estimator.inputs.pandas_input_fn(test_data[feature_names], shuffle=False)
-classifier.predict()
+test_input = tf.estimator.inputs.pandas_input_fn(x=test_data[feature_names], shuffle=False, batch_size=100)
+ypreds = list(classifier.predict(input_fn=test_input))
+
+namepreds = [pred['classes'][0].decode('UTF-8') for pred in ypreds]
+
+pred_list_with_id = list(zip(test_data.id.values, namepreds))
+
+with open(current_dir + 'tf1.csv', 'w') as f:
+    f.write('id,status_group\n')
+    for row in pred_list_with_id:
+        f.write('{},{}\n'.format(row[0],row[1]))

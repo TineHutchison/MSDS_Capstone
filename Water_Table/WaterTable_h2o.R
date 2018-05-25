33db79c10fc8591ea23967d8957eb3ffb82ee10e
Dan <mntdewdan+coursera@gmail.com>
2018-05-25 01:17:22 -0700

    Updated h2o random forest

--- Water_Table/WaterTable_h2o.R
+++ Water_Table/WaterTable_h2o.R
@@ -1,35 +1,35 @@
 ################# START: variables and libraries for submission information
-
-numberOfTrees <- 5000
-submissionName <- "Random Forest Simple with missing and imputed elevation added, funder category"
-
 library("randomForest")
 library("caret")
 library("e1071")
-?library
 library("lubridate")
 library("h2o" )
-h2o.getVersion()
+
 ################# END: variables and libraries for submission information
 
 #source("WaterTable_LoadAndClean.R")
-
+source("WaterTable_LoadAndClean_v2.R")
+set.seed(400)
 RandomForestFieldList <<- c(
   #"id"
-  #,"amount_tsh"
-  #,"has_amount_tsh"
+  #"amount_tsh"
+  #,
+  "log_amount_tsh"
+  #,
+  ,"has_amount_tsh"
   
   
-  "date_recorded"
+  #,
+  ,"date_recorded"
   ,"monthRecorded"
   
   #### funder or funder_cat. 
-  #,"funder"
+  ,"funder"
   #,"funder_cat"     
   
   
   ## either gps_height, elevation, gps_height_mice or elevation2
-  #,"has_gps_height"    # 1 if gps_height != 0
+  ,"has_gps_height"    # 1 if gps_height != 0
   #,"gps_height"
   #,"elevation"
   #,"missing_elevation"    #if gps_height == 0 and it can't be imputed based on GPS coordinates
@@ -61,7 +61,7 @@ RandomForestFieldList <<- c(
   ### other location based fields
   ,"basin"
   #,"subvillage"
-  #,"lga"
+  ,"lga"
   #,"ward"
   
   
@@ -80,19 +80,19 @@ RandomForestFieldList <<- c(
   
   
   
-  #,"installer"
+  ,"installer"
   #,"wpt_name"
   ,"num_private"
   ,"public_meeting"
   #,"recorded_by"
   ,"scheme_management"
-  #,"scheme_name"
+  ,"scheme_name"
   ,"permit"
-  #,"extraction_type"
+  ,"extraction_type"
   ,"extraction_type_group"
   ,"extraction_type_class"
   ,"management"
- # ,"management_group"
+  ,"management_group"
   
   #### only need payment or payment_type since they're a 1-to-1 match
   ,"payment"
@@ -111,33 +111,28 @@ RandomForestFieldList <<- c(
   ,"source_class"
   ,"waterpoint_type"
   ,"waterpoint_type_group"
-  #,"funder_sz"
   
   #### whether cpg has any missing data
   #,"has_cpg_missing_data"    # missing all construction_year, population, gps_height
   #,"has_cpg_some_data"       # missing some of the fields construction_year, population, gps_height
+  #,"funder_sz"
+  #,"has_scheme_name"
 )
+
+
 randomForest_data_train <- water_table_train[,RandomForestFieldList]
 randomForest_data_holdout <- water_table_holdout[,RandomForestFieldList]
-randomForest_data_full <- water_table[,RandomForestFieldList]
-randomForest_data_test <- water_table_test[,RandomForestFieldList]
 
 randomForest_data_train$days <- as.integer(as.Date("14/01/01", format="%y/%m/%d") - randomForest_data_train$date_recorded )
 randomForest_data_holdout$days <- as.integer(as.Date("14/01/01", format="%y/%m/%d") - randomForest_data_holdout$date_recorded) 
-randomForest_data_full$days <- as.integer(as.Date("14/01/01", format="%y/%m/%d") - randomForest_data_full$date_recorded )
-randomForest_data_test$days <- as.integer(as.Date("14/01/01", format="%y/%m/%d") - randomForest_data_test$date_recorded )
-
-as.integer()
-class(as.integer(randomForest_data_train$days))
-?as.Date
-?Date
-summary(randomForest_data_train)
 
 
-h2o.init(nthreads=-1,max_mem_size='16G')
-#h2o.shutdown(prompt=FALSE)
+library(h2o)
+h2o.shutdown(prompt=FALSE)
+Sys.sleep(5)
+h2o.init(nthreads=-1)
+
 set.seed(42)
-
 
 
 
@@ -145,51 +140,55 @@ water_table_train_h2o <- cbind(randomForest_data_train, water_table_train_y$stat
 water_table_holdout_h2o <- cbind(randomForest_data_holdout, water_table_holdout_y$status_group) 
 
 colnames(water_table_train_h2o)[ncol(water_table_train_h2o)] <- "status_group"
-#remove(features)
 colnames(water_table_holdout_h2o)[ncol(water_table_train_h2o)] <- "status_group"
-
-
-#importance(randomForest_Training)
-#confusionMatrix(water_table_holdout_y$status_group, predict(randomForest_Training, water_table_holdout))
 
 trainH2o<-as.h2o(water_table_train_h2o)
 features<-colnames(trainH2o)
-#features[35] <- "status_group"
 
 holdoutH2o<-as.h2o(water_table_holdout_h2o)
-features<-colnames(holdoutH2o)
-#features[35] <- "status_group"
-summary(holdoutH2o)
-####### run full model and write results to file
-#set.seed(42)
-#h2o.getFrame(water_table)
-randomForest_AllData <- h2o.randomForest(features,mtries=16,"status_group", trainH2o, ntrees=1000)
-?h2o.randomForest
-randomForest_AllData
-summary(randomForest_AllData)
-predictionH2o <- predict(randomForest_AllData, holdoutH2o)
-colnames(as.data.frame(predictionH2o$predict))
+randomForesth2o_trainData <- h2o.randomForest(features,"status_group", trainH2o, ntrees=1000, mtries=9, max_depth=22, nbins=22, seed=42)
+summary(randomForesth2o_trainData)
+h2o.varimp_plot(randomForesth2o_trainData)
+
+
+
+predictionH2o <- predict(randomForesth2o_trainData, holdoutH2o)
 confusionMatrix(as.data.frame(predictionH2o$predict)$predict, water_table_holdout_y$status_group)
+
+vip <- as.data.frame(h2o.varimp(randomForesth2o_trainData))
+print(vip)
+
+
+
+
+
+
+
+
+
+#### For file for submission
+randomForest_data_full <- water_table[,RandomForestFieldList]
+randomForest_data_test <- water_table_test[,RandomForestFieldList]
+randomForest_data_full$days <- as.integer(as.Date("14/01/01", format="%y/%m/%d") - randomForest_data_full$date_recorded )
+randomForest_data_test$days <- as.integer(as.Date("14/01/01", format="%y/%m/%d") - randomForest_data_test$date_recorded )
 
 
 water_table_h2o <- cbind(randomForest_data_full, water_table_y$status_group)
 water_table_test_h2o <- cbind(randomForest_data_test) 
 
 colnames(water_table_h2o)[ncol(water_table_h2o)] <- "status_group"
-#remove(features)
-#colnames(water_table_test_h2o)[ncol(water_table_test_h2o)] <- "status_group"
+
 
 fullH2o<-as.h2o(water_table_h2o)
 features<-colnames(fullH2o)
 testH2o <- as.h2o(water_table_test_h2o)
 
-summary(randomForesth2o_AllData)
-randomForesth2o_AllData <- h2o.randomForest(features,mtries=16,"status_group", fullH2o, ntrees=2500)
-
- 
+#summary(randomForesth2o_AllData)
+### Current best - randomForesth2o_AllData <- h2o.randomForest(features,mtries=9, max_depth=22, nbins=22,"status_group", fullH2o, ntrees=2500, seed=42)
+### Test #2 - randomForesth2o_AllData <- h2o.randomForest(features,mtries=9, max_depth=21, nbins=21,"status_group", fullH2o, ntrees=2500, seed=42)
+#### Original - randomForesth2o_AllData <- h2o.randomForest(features,mtries=16,"status_group", fullH2o, ntrees=2500, seed=42)
+randomForesth2o_AllData <- h2o.randomForest(features,mtries=9, max_depth=21, nbins=21,"status_group", fullH2o, ntrees=2500, seed=42)
 predictionH2o <- predict(randomForesth2o_AllData, testH2o)
-colnames(as.data.frame(predictionH2o$predict))
 
-
-writeFile("Random Forest (h2o) Simple with missing and imputed elevation added, trees=2500", as.data.frame(predictionH2o$predict))
+writeResultsFileForSubmission("Winning model - Random Forest (h2o) Simple added (+high factor vars) and post grid search (21-9-21), trees=2500", as.data.frame(predictionH2o$predict))
 
